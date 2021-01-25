c24248ac2767b416f5e84fd47ec3e37d79dfd2a5
renianjadeubamos <77970261+renianjadeubamos@users.noreply.github.com>
2021-01-25 05:15:26 -0800

    Update winx.php

--- winx.php
+++ winx.php
@@ -1,14 +1,14 @@
 <html>	
-   <Head><title> WEBPAGE </title></head>
+   <Head><title> WEBPAGE </title> </Head>
     <body background="power.jpg"> 
     
 <body>
 <FONT COLOR= "GreenYellow", SIZE= 25, FACE= Brush Script MT>
-<p style= "text-align:center"><b> Pandemic </p></b>
+<p style= "text-align:center"><b> Pandemic </P> </B>
 <br>
 <br>
-<p style= "text-align:center"><img src = winx.jpg , width = 900 , length = 900> </p></img>
+<p style= "text-align:center"><img src = winx.jpg , width = 900 , length = 900> </P> </IMG>
 <FONT COLOR= "DeepSkyBlue", SIZE= 25, FACE= Brush Script MT>
 <p><marquee behavior= scroll, direction= right> "Don't Live In Fear Live In Faith"<marquee>
-</body>
-</html>
+</Body>
+</HTML>
